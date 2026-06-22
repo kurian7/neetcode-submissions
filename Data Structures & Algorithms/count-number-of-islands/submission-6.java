6b3d89e9cc2d653688db4657653efe2611f533ce
kurian7 <70152750+kurian7@users.noreply.github.com>
2026-06-22 15:57:48 +0530

    Add: count-number-of-islands - submission-6

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+       if(grid.length == 0)
+         return -1;
+       int row = grid.length;
+       int col = grid[0].length;
+       int island = 0;
+       boolean[][] visited = new boolean[row][col];
+       for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(grid[i][j] == '1' && !visited[i][j]){
+                bfs(grid, i, j, visited);
+                island++;
+            }
+        }
+       } 
+       return island;  
+    }
+    private void bfs(char[][] grid, int r, int c, boolean[][] visited){
+        Queue<int[]> q =new LinkedList<>();
+        q.offer(new int[]{r,c});
+        visited[r][c] = true;
+        while(!q.isEmpty()){
+            int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+            int[] co = q.poll();
+            for(int[] d : dir){
+                int dr = d[0]+co[0];
+                int dc = d[1] +co[1];
+                if(dr>=0 && dc>=0 && dr<grid.length && dc<grid[0].length && grid[dr][dc] == '1' && !visited[dr][dc]){
+                    q.offer(new int[]{dr,dc});
+                    visited[dr][dc] = true;
+                }
+            }
+        }
+    }
+}
